6f2f0d9485bac34c169f88ff4f0bf71de552fe4c
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-03-18 14:58:10 +0100

    userDefaults example

--- UserDefaultsTest/ViewController.swift
+++ UserDefaultsTest/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var label: UILabel!
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func sendButtonPressed(_ sender: UIButton) {
+        if textField.text!.count > 0
+        {
+       defaults.set(textField.text, forKey: "Tekst")
+        }
+    }
+    
+    @IBAction func loadButtonPressed(_ sender: UIButton) {
+        label.text = "Stored in userDefaults: \(defaults.string(forKey: "Tekst") ?? "-")"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        defaults.removeObject(forKey: "Tekst")
+        label.text = "Stored in userDefaults: -"
+    }
 }
 
